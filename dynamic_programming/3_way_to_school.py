dc38fb3e1bb241e0e5041001c93a579310dc7fa8
ug-kim <ugkim816@gmail.com>
2020-10-02 03:11:32 +0900

    [#13] way to school

---
+++ dynamic_programming/3_way_to_school.py
@@ -0,0 +1,16 @@
+def solution(m, n, puddles):
+    way = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+    # start
+    way[1][1] = 1
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1 and j == 1:
+                continue
+            if [j, i] in puddles:
+                way[i][j] = 0
+            else:
+                way[i][j] += (way[i-1][j] + way[i][j-1])
+
+    return way[-1][-1] % 1000000007
